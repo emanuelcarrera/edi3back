a418cf6eaed2acf4b77c97392e9147fc53a6948b
emanuelcarrera <68482024+emanuelcarrera@users.noreply.github.com>
2021-06-26 21:11:25 -0300

    cas

--- app/accesoADatos/AccesoDatos.php
+++ app/accesoADatos/AccesoDatos.php
@@ -11,7 +11,7 @@ class AccesoDatos
        //
        
        //$this->objetoPDO = new PDO('mysql:host='.getenv('ServidorMySQL').';dbname='.getenv('Database').';charset=utf8', getenv("Usuario"), getenv('Pass'), array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-        $this->objetoPDO = new PDO('mysql:host=localhost;dbname=edi3colecciones', 'root', '');
+        $this->objetoPDO = new PDO('mysql:host=localhost;dbname=edi3colecciones;charset=utf8', 'root', '');
             $this->objetoPDO->exec("SET CHARACTER SET utf8");
         } catch (PDOException $e) {
             print "Error: " . $e->getMessage();
